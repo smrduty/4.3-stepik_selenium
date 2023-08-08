42852ac07929240f32ea22b81379bc218b83f544
smrduty <71096027+smrduty@users.noreply.github.com>
2023-08-08 23:18:58 +0300

    Add files via upload
    stepik 4.3 step 14

--- locators.py
+++ locators.py
@@ -1,5 +1,9 @@
 from selenium.webdriver.common.by import By
 
+class MainPageLocators():
+
+	LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 class BasePageLocators():
 
 	LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -29,6 +33,20 @@ class BasketPageLocators():
 	BASKET_IS_NOT_EMPTY = (By.CLASS_NAME, "basket-items")
 
 class LoginPageLocators():
+
+	LOGIN_FORM = (By.ID, "login_form")
+
+    EMAIL_LOGIN_INPUT = (By.CSS_SELECTOR, '[name="login-username"]')
+
+    PASSWORD_LOGIN_INPUT = (By.CSS_SELECTOR, '[name="login-password"]')
+
+    REGISTER_FORM = (By.ID, "register_form")
+    
+    EMAIL_REGISTER_INPUT = (By.CSS_SELECTOR, '[name="registration-email"]')
+    
+    PASSWORD_REGISTER_INPUT = (By.CSS_SELECTOR, '[name="registration-password1"]')
+    
+    PASSWORD_REGISTER_INPUT_AGAIN = (By.CSS_SELECTOR, '[name="registration-password2"]')
 
 	EMAIL = (By.ID, "id_registration-email")
 
